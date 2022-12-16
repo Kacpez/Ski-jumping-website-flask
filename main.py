9d44f16ac1a5914adea2ea520897fcbc60ab54a8
Kacpez <pokacper.goraj@interia.pl>
2022-12-16 13:12:50 +0100

    Działający program

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask,jsonify, render_template, request, flash, redirect, url_for
 import os
+from neo4j import GraphDatabase
+
+
+uri = "neo4j+s://89d05a8e.databases.neo4j.io"
+user = "neo4j"
+password = "6x_sHNzaRO5iM-5fOjnqCcd0EUPV528bKyQxwmLCWxg"
+
+driver=GraphDatabase.driver(uri=uri,auth=(user,password))
+session=driver.session()
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
